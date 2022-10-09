236b634d7716e1707139e3f78ae6fc25184b89e5
Faaaronn <jakubjansojecki@gmail.com>
2022-10-09 19:39:40 +0200

    separate merge and zip endpoints

--- src/contracts/contracts.controller.ts
+++ src/contracts/contracts.controller.ts
@@ -56,4 +56,37 @@ export class ContractsController {
 
     res.end(zippedContracts);
   }
+
+  @Post('/merge')
+  async mergePDFs(@Res() res: Response, @Body() contractData: any) {
+    const contracts = await this.contractsService.mergeContracts(contractData);
+    res.set({
+      // pdf
+      'Content-Type': 'application/pdf',
+      'Content-Disposition': `attachment; filename=aplugks.pdf`,
+      'Content-Length': contracts.length,
+      'Access-Control-Expose-Headers': 'Content-Disposition',
+      'Cache-Control': 'no-cache, no-store, must-revalidate',
+      Pragma: 'no-cache',
+      Expires: 0,
+    });
+
+    res.end(contracts);
+  }
+  @Post('/zip')
+  async zipPDFs(@Res() res: Response, @Body() contractData: any) {
+    const zippedContracts = await this.compressionService.zipFiles(
+      contractData,
+    );
+    res.set({
+      'Content-Type': 'application/zip',
+      'Content-Disposition': `attachment; filename=aplugks.zip`,
+      'Content-Length': zippedContracts.length,
+      'Cache-Control': 'no-cache, no-store, must-revalidate',
+      Pragma: 'no-cache',
+      Expires: 0,
+    });
+
+    res.end(zippedContracts);
+  }
 }
