26eaa119c429ae87dcfa877b5e3f6d640bc653ea
Faaaronn <jakubjansojecki@gmail.com>
2022-10-09 16:54:21 +0200

    cors

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,21 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  const whitelist = [
+    'http://localhost:3000',
+    'https://mellow-donut-c93449.netlify.app/',
+  ];
+
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: function (origin, callback) {
+      if (!origin || whitelist.indexOf(origin) !== -1) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
+  });
   await app.listen(process.env.PORT || 1010);
 }
 bootstrap();
